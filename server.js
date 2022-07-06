b8b1ee2f9a95619589ad909b592c2635dcc99293
Andre Smith <andre.frank.smith@gmail.com>
2022-07-06 07:30:10 +0200

    Implemented file download functionality

--- server.js
+++ server.js
@@ -28,9 +28,17 @@ app.post("/upload", upload.single("file"), async (req, res) => {
 
   const file = await File.create(fileData);
 
-  console.log(file);
+  res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` });
+});
 
-  res.send(file.originalName);
+app.get("/file/:id", async (req, res) => {
+  const file = await File.findById(req.params.id);
+
+  file.downloadCount++;
+  await file.save();
+  console.log(file.downloadCount);
+
+  res.download(file.path, file.originalName);
 });
 
 app.listen(3000);
